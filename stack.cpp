8c16b1d7eab9611ecb4a491dd6fc297363d1d85b
arybutler <andrewrybutler@gmail.com>
2017-03-02 18:50:00 -0800

    Adding stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <assert.h>
+#include "stack.h"
+
+
+//Andrew Butler
+//stack.cpp
+
+
+
+Stack::Stack()
+{}
+
+Stack::~Stack()
+{
+  //while(!IsEmpty())
+  {
+      double n = TopPtr.FirstNode();
+      TopPtr.RemoveNode();
+      }
+}
+
+void Stack::Push(double n)
+{
+  TopPtr.AddNode(n);
+}
+
+double Stack::Pop()
+{
+  TopPtr.RemoveNode();
+}
+
+int Stack::IsEmpty()
+{
+  int n = TopPtr.Size();
+  if(n == 0)
+    {
+      return 1;
+    }
+  else
+    {
+      return 0;
+    }
+}
+
+void Stack::Print()
+{
+  TopPtr.PrintList();
+}
